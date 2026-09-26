d4a0dd753a5fb671b7a67b9caee5a903d39c3e8c
Eric Hu <38444465+ehugithub@users.noreply.github.com>
2026-09-26 01:43:05 -0400

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countComponents(self, n: int, edges: list[list[int]]) -> int:
+        adj = [[] for _ in range(n)]
+
+        count = 0
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        not_seen = set(range(n))
+        seen = set()
+
+        def dfs(node, parent):
+            seen.add(node)
+            not_seen.remove(node)
+
+            for nbr in adj[node]:
+                if nbr == parent:
+                    continue
+                if nbr in not_seen:
+                    dfs(nbr, node)
+        
+        while not_seen:
+            unseen = not_seen.pop()
+            not_seen.add(unseen)
+            
+            dfs(unseen, -1)
+            count += 1
+
+        return count
+
+        
